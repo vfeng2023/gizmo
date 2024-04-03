75ab2e6f9ca626cc9aa06bd19d6810ba7763a029
vfeng2023 <vfeng2023@gmail.com>
2024-04-03 16:14:46 -0400

    created images

--- gizmo.py
+++ gizmo.py
@@ -36,14 +36,34 @@ outsidetemp.grid(row=4, column=0)
 otempentry = tk.Entry(button_frame, width=10)
 otempentry.grid(row=4, column=1)
 
+startbutton = tk.Button(button_frame, text="Start", bg="green")
+startbutton.grid(row=5,column=0)
+
+stopbutton = tk.Button(button_frame, text="Stop", bg="red")
+stopbutton.grid(row=5, column=1)
+
+resetbutton = tk.Button(button_frame, text="Reset", bg="Yellow")
+resetbutton.grid(row=5, column=2)
 # add insulator image
 insulator = tk.Canvas(root)
 insulator.configure(height=400, width=400)
-insulator.configure(bg="red")
+# insulator.configure(bg="red")
+# insulator.create_text(200,200, text="Insulator graph panel")
+
+#placeholder image for low-fidelity prototype
+imginsulator = tk.PhotoImage(file="insulator.png")
+insulator.create_image(200,200,image=imginsulator)
+# placeholder image for low-fidelity prototype
 insulator.grid(row=1)
 # add temperature graph
 
 tempgraph = tk.Canvas(root)
-tempgraph.configure(height=400, width=400,bg="blue")
+tempgraph.configure(height=400, width=400)
+# tempgraph.create_text(200, 200, text="Temperature graph")
+img2 = tk.PhotoImage(file="graph.png")
+
+#placeholder image for low-fidelity prototype
+tempgraph.create_image(200,200,image=img2)
 tempgraph.grid(row=0,column=1)
+
 root.mainloop()
